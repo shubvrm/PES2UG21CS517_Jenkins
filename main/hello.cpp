28341755dd8118fdd191d8a6a85d0a139a6b7654
Abdullah Amal Khan <abdumanakh04@outlook.com>
2024-03-18 21:02:20 +0530

    Commit 2

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,5 +6,6 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run\n";
+  cout << "I am Shubhika Verma, P{ES2UG21CS517\n";
   return 0;
 }
